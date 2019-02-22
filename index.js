9f6c96cdcc886f48688adbb6d1761f05d496ee9c
Michal Caninec <35670429+MajklCan@users.noreply.github.com>
2019-02-22 09:11:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,10 @@
-const express = require('express');
-const exhbs = require('express-handlebars');
-const con = require('./db.js');
-const cors = require('cors');
-//const bodyParser = require('body-parser')
-
-const app = express();
-app.use(cors())
-
-//app.use(bodyParser.json())
-
-
-
- app.engine('handlebars', exhbs({defaultLayout: 'main'}));
- app.set('view engine', 'handlebars');
-
- app.get('/', function (req, res) {
-    res.send("Success"); 
- }); 
- app.get('/index', function (req, res) {
-     res.send("Success");  
-}); 
-
- app.get('/user/:id', function (req, res) {
-    con.query("SELECT * FROM customers WHERE cust_id="+req.params.id+
-    ";SELECT ord_ite.orde_id, customers.cust_id, items.item_id, orders.orde_date, items.name, SUM(ord_ite.price) AS sum_price FROM orders INNER JOIN ord_ite ON ord_ite.orde_id = orders.orde_id INNER JOIN customers ON customers.cust_id = orders.cust_id INNER JOIN items ON items.item_id = ord_ite.item_id GROUP BY ord_ite.orde_id, items.name HAVING customers.cust_id = "+
-    req.params.id,
-    (err,result)=>{
-        res.render('user',{"data": result})
-        //res.json({"data": result});
-    }) 
-}); 
-
-app.get('/UserList', function (req, res) {
-    con.query("SELECT * FROM customers",(err,result)=>{
-        //res.render('index',{"data": result,"login": err})
-        res.json(result);
-    }) 
- });
-
-/* app.get('/deleteUser/:id', function (req, res) {
-    con.query("SELECT * FROM users",(err,result)=>{
-       // res.render('index',{"data": result,"input":true})
-       console.log(req.params.id)
-       con.query("DELETE FROM users WHERE id_user ="+req.params.id,(err,result)=>{
-            res.redirect('http://localhost:5500/');
-        })
-       res.end()
-    }) 
-});  */
-
-
-app.listen(8080);
+const http = require('http');
+const port=process.env.PORT || 3000
+const server = http.createServer((req, res) => {
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/html');
+res.end('<h1>Hello World</h1>');
+});
+server.listen(port,() => {
+console.log(`Server running at port `+port);
+});
